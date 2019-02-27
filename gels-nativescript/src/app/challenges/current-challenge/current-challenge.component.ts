9fefb0b5efb1127b62eb39db0395ccc5ac3b6289
IamGiel <november14giel@gmail.com>
2019-02-27 16:38:16 -0500

    creating own view and setting up app module, set current-challenge.component

---
+++ gels-nativescript/src/app/challenges/current-challenge/current-challenge.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "ns-current-challenge",
+  templateUrl: "./current-challenge.component.html",
+  styleUrls: ["./current-challenge.component.css"],
+  moduleId: module.id
+})
+export class CurrentChallengeComponent {}
